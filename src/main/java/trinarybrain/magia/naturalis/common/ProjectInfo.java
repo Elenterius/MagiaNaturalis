330de81cf64279523d24cb490f5882e8b1749462
TrinaryBrain <trinarybrain@gmail.com>
2015-01-17 01:51:36 +0100

    update version

--- src/main/java/trinarybrain/magia/naturalis/common/ProjectInfo.java
+++ src/main/java/trinarybrain/magia/naturalis/common/ProjectInfo.java
@@ -4,7 +4,7 @@ public final class ProjectInfo
 {
 	public static final String ID = "MagiaNaturalis";
 	public static final String NAME = "Magia Naturalis";
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.1.1";
 	public static final String MC_VERSION = "[1.7.10]";
 	public static final String DEPENDENCIES = "required-after:Thaumcraft";
 	public static final String COMMON_PROXY = "trinarybrain.magia.naturalis.common.core.CommonProxy";
