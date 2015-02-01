fd7046e1ff7c5143d683996271c9221f05bd5bd3
TrinaryBrain <trinarybrain@gmail.com>
2015-02-01 17:43:51 +0100

    added cost calc

--- src/main/java/trinarybrain/magia/naturalis/common/tile/TileGeoMorpher.java
+++ src/main/java/trinarybrain/magia/naturalis/common/tile/TileGeoMorpher.java
@@ -1,9 +1,18 @@
 package trinarybrain.magia.naturalis.common.tile;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeDictionary.Type;
 import thaumcraft.api.TileThaumcraft;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+import thaumcraft.common.lib.world.biomes.BiomeHandler;
 import trinarybrain.magia.naturalis.common.core.Log;
 import trinarybrain.magia.naturalis.common.util.Platform;
 import trinarybrain.magia.naturalis.common.util.WorldUtil;
@@ -13,6 +22,8 @@ public class TileGeoMorpher extends TileThaumcraft
 	public int ticks = 0;
 	private int morphX = 0;
 	private int morphZ = 0;
+	private BiomeGenBase lastBiome = null;
+	private AspectList morphCost = null;
 
 	@Override
 	public void updateEntity()
@@ -28,7 +39,7 @@ public class TileGeoMorpher extends TileThaumcraft
 	public boolean handleBiomeMorphing(int radius)
 	{
 		ticks = 0;
-		BiomeGenBase biome = BiomeGenBase.jungle;
+		BiomeGenBase newBiome = BiomeGenBase.jungle;
 
 		if(this.morphZ < radius * 2)
 		{
@@ -44,9 +55,20 @@ public class TileGeoMorpher extends TileThaumcraft
 
 			int posX = this.xCoord - radius + this.morphX;
 			int posZ = this.zCoord - radius + this.morphZ;
-			if(this.worldObj.getBiomeGenForCoords(posX, posZ) != biome && (Math.abs(morphX-radius) + Math.abs(morphZ-radius)) <= radius)
+			BiomeGenBase oldBiome = this.worldObj.getBiomeGenForCoords(posX, posZ);
+			
+			if(newBiome != oldBiome && (Math.abs(morphX-radius) + Math.abs(morphZ-radius)) <= radius)
 			{
-				WorldUtil.setBiomeAt(this.worldObj, posX, posZ, biome);
+				if(this.lastBiome != oldBiome) //prevent recalculation of Cost every Cycle
+				{
+					this.lastBiome = oldBiome;
+					this.morphCost = this.calculateMorphCost(oldBiome, newBiome);
+					Log.logger.info("Recalculated Morph Cost!");
+				}
+		
+				Log.logger.info(this.morphCost);
+				
+				WorldUtil.setBiomeAt(this.worldObj, posX, posZ, newBiome);
 				this.worldObj.getChunkFromBlockCoords(this.xCoord, this.zCoord).setChunkModified();
 				Minecraft.getMinecraft().theWorld.markBlockForUpdate(posX, this.worldObj.getTopSolidOrLiquidBlock(posX, posZ), posZ); //Will only work in SSP
 				return true;
@@ -59,6 +81,88 @@ public class TileGeoMorpher extends TileThaumcraft
 			ticks = 20;
 		}
 		return false;
+	}
+	
+	public AspectList calculateMorphCost(BiomeGenBase oldBiome, BiomeGenBase newBiome)
+	{  	    
+		BiomeDictionary.Type[] oldTypes = BiomeDictionary.getTypesForBiome(oldBiome);
+		BiomeDictionary.Type[] newTypes = BiomeDictionary.getTypesForBiome(newBiome);
+		
+		AspectList aspectCost = new AspectList();
+		
+		// Remove Equal Biome Types
+	    for(int i = 0; i < oldTypes.length; i++)
+	    {
+	    	for(int j = 0; j < newTypes.length; j++)
+	    	{
+	    		if(oldTypes[i] == newTypes[j])
+	    		{
+	    			oldTypes[i] = newTypes[j] = null;
+	    			break;
+	    		}
+	    	}
+	    }
+	    
+	    // Add AspectCost for Removing old Biome Types
+	    for(int i = 0; i < oldTypes.length; i++)
+	    {
+	    	if(oldTypes[i] != null)
+	    	{
+	    		if(oldTypes[i] != Type.MAGICAL) // Because Thaumcraft adds null aspect for magical biome type
+	    		{
+	    			Aspect aspect = (Aspect)((List)BiomeHandler.biomeInfo.get(oldTypes[i])).get(1);
+	    			if(aspect != null)
+	    			{
+	    				int aura = (int)((List)BiomeHandler.biomeInfo.get(oldTypes[i])).get(0);
+						aspectCost.add(aspect, aura);
+	    			}
+	    		}
+	    		else
+	    		{
+	    			aspectCost.add(Aspect.MAGIC, 100);
+	    		}
+	    	}
+	    }
+	    
+	    // Add MorphCost for adding Biome Types & reduce by difference of aspects that will remain in biome
+	    for(int i = 0; i < newTypes.length; i++)
+	    {
+	    	if(newTypes[i] != null)
+	    	{
+	    		if(newTypes[i] != Type.MAGICAL) // Because Thaumcraft adds null aspect for magical biome type
+	    		{
+	    			Aspect aspect = (Aspect)((List)BiomeHandler.biomeInfo.get(newTypes[i])).get(1);
+	    			if(aspect != null)
+	    			{
+	    				int aura = (int)((List)BiomeHandler.biomeInfo.get(newTypes[i])).get(0);
+	    				
+	    				int currentCost = aspectCost.getAmount(aspect);
+	    				if(currentCost > 0)
+	    				{
+	    					aspectCost.reduce(aspect, Math.abs(currentCost - aura));
+	    				}
+	    				else
+	    				{
+	    					aspectCost.add(aspect, aura);
+	    				}
+	    			}
+	    		}
+	    		else
+	    		{
+	    			int currentCost = aspectCost.getAmount(Aspect.MAGIC);
+    				if(currentCost > 0)
+    				{
+    					aspectCost.reduce(Aspect.MAGIC, Math.abs(currentCost - 100));
+    				}
+    				else
+    				{
+    					aspectCost.add(Aspect.MAGIC, 100);
+    				}
+	    		}
+	    	}
+	    }
+	    
+	    return aspectCost;
 	}
 
 	@Override
