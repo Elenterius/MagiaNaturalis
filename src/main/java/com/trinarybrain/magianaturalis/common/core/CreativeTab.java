b1ee9bd4f050eb3edbc8943fe9b155612aef54f1
TrinaryBrain <trinarybrain@gmail.com>
2015-11-21 03:40:35 +0100

    fix wrong packaging build

--- src/main/java/com/trinarybrain/magianaturalis/common/core/CreativeTab.java
+++ src/main/java/com/trinarybrain/magianaturalis/common/core/CreativeTab.java
@@ -1,10 +1,11 @@
 package com.trinarybrain.magianaturalis.common.core;
 
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
 import com.trinarybrain.magianaturalis.common.item.ItemsMN;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 
 public class CreativeTab extends CreativeTabs
 {
