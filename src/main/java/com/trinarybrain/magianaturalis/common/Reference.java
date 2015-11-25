ea7c791004a183ade94bb90f885f7f96cbe3dcf0
TrinaryBrain <trinarybrain@gmail.com>
2015-11-25 22:45:46 +0100

    Version Bump - v0.1.4

--- src/main/java/com/trinarybrain/magianaturalis/common/Reference.java
+++ src/main/java/com/trinarybrain/magianaturalis/common/Reference.java
@@ -4,7 +4,7 @@ public final class Reference
 {
 	public static final String ID = "magianaturalis";
 	public static final String NAME = "Magia Naturalis";
-	public static final String VERSION = "0.1.3";
+	public static final String VERSION = "0.1.4";
 	public static final String MC_VERSION = "[1.7.10]";
 	public static final String DEPENDENCIES = "required-after:Thaumcraft";
 	public static final String COMMON_PROXY = "com.trinarybrain.magianaturalis.common.core.CommonProxy";
